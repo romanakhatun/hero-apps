0764db015be0a0f542bbbc5875a81e6787d1f6e1
romanakhatun <romanakhatun2004@gmail.com>
2025-10-08 09:12:47 +0600

    project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [],
+    // errorElement: <ErrorPage />,
+  },
+]);
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>im header</div>;
+};
+
+export default Navbar;
